1a37450cc5ba964ffe3244d55f0ac504f04f97bd
Ambani Matsedu <ambani.matsedu@gmail.co.za>
2019-03-13 12:02:55 +0200

    Adds book-map and pipes

--- src/app/common/map/map.component.ts
+++ src/app/common/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MapService } from './map.service';
 
 @Component({
   selector: 'buka-map',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  @Input() location : string;
+
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  constructor(private mapService: MapService) { }
 
   ngOnInit() {
+  }
+
+  mapReadyHandler(){
+    this.mapService.geocodeLocation(this.location).subscribe(
+      (coordinates) => {
+        this.lat = coordinates.lat;
+        this.lng = coordinates.lng;
+      }
+    );
   }
 
 }
